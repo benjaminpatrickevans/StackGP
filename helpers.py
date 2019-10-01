f1c8204f2737221d54b3edbe341185014a408e95
Ben Evans <ben.evans_9@hotmail.com>
2019-10-01 14:43:51 +1300

    Refactoring to allow straight comparisons to other methods

--- helpers.py
+++ helpers.py
@@ -1,10 +1,64 @@
-
 import pandas as pd
 import numpy as np
 import pickle
-from timeit import default_timer as timer
 from sklearn import metrics
-from sklearn.preprocessing import OneHotEncoder
+from src import sklearn_additions
+from sklearn.model_selection import KFold
+import time
+import sys
+
+num_folds = 10
+
+def evaluation_score(real_y, predicted_y):
+    return metrics.f1_score(real_y, predicted_y, average="weighted")
+
+
+def run_and_time_classifier(model, train_x, train_y, test_x, test_y):
+    start_time = time.time()
+    model.fit(train_x, train_y)
+    training_time_seconds = time.time() - start_time
+    training_time_minutes = int(training_time_seconds / 60)
+
+    predictions = model.predict(test_x)
+    score = evaluation_score(test_y, predictions)
+
+    return model, training_time_minutes, score
+
+
+def is_number(x):
+    try:
+        float(x)
+        return True
+    except ValueError:
+        return False
+
+
+def _categorical_features(data_x):
+    '''
+        Returns the categorical column names
+        from data_x. Returns an empty set
+        if they are all numeric.
+    :param data_x:
+    :return:
+    '''
+
+    categorical_features = []
+
+    for col in data_x:
+        values = np.unique(data_x[col].values)
+
+        # If they are all numeric
+        numeric = np.all([is_number(x) for x in values])
+
+        if not numeric:
+            categorical_features.append(col)
+
+    return categorical_features
+
+
+def _fill_missing(x):
+    imputer = sklearn_additions.DataFrameImputer()
+    return imputer.fit_transform(x)
 
 
 def read_data(data_path, class_index="last"):
@@ -15,14 +69,25 @@ def read_data(data_path, class_index="last"):
 
     # Most datasets have class as either the first or last column
     if class_index == "last":
-        dataX = data[:, :-1]
-        dataY = data[:, -1].astype(str)  # Class label is a string
+        data_x = data[:, :-1]
+        data_y = data[:, -1]
     else:
-        dataX = data[:, 1:]
-        dataY = data[:, 0].astype(str)  # Class label is a string
+        data_x = data[:, 1:]
+        data_y = data[:, 0]
 
+    data_x = pd.DataFrame(data_x)
+    data_y = pd.DataFrame(data_y, dtype=str)
 
-    return pd.DataFrame(dataX, dtype=float), pd.DataFrame(dataY)
+    # First thing we need to do is deal with missing values
+    data_x = _fill_missing(data_x)
+
+    # Categorical features are those where all the values are not numeric
+    categorical_features = _categorical_features(data_x)
+
+    # Convert any categorical values to numeric
+    data_x = pd.get_dummies(data_x, columns=categorical_features)
+
+    return pd.DataFrame(data_x, dtype=float), data_y
 
 
 def write_file(file, contents):
@@ -30,13 +95,38 @@ def write_file(file, contents):
         pickle.dump(contents, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-def evaluate(model, trainX, trainY, testX, testY):
-    start_time = timer()
+def run_kfold(k, dataX, dataY, fn_to_run, n_splits=10):
+    print("Fold:", k)
+    print("-" * 10)
 
-    model.fit(trainX, trainY)
+    # 10-fold. Shuffle the data according to the fixed seed for reproducability
+    kf = KFold(n_splits=n_splits, shuffle=True, random_state=0)
 
-    training_time = (timer() - start_time) * 1000 # Convert to milliseconds
+    # We only care about the Kth fold - we run each fold as different process for speed of results
+    train, test = list(kf.split(dataX, dataY))[k]
 
-    predictions = model.predict(testX)
-    f_measure = metrics.f1_score(testY, predictions, average='weighted')
-    return f_measure, training_time
+    train_x, train_y = dataX.iloc[train], dataY.iloc[train]
+    test_x, test_y = dataX.iloc[test], dataY.iloc[test]
+
+    model, training_time_minutes, score = fn_to_run(train_x, train_y, test_x, test_y)
+
+    return score
+
+def run(dataset, class_index, k, fn_to_run):
+    data_x, dataY = read_data(dataset, class_index)
+    print("Data Read")
+    return run_kfold(k, data_x, dataY, fn_to_run)
+
+
+def main(fn_to_run, data_folder="datasets/uci/"):
+    if len(sys.argv) != 4:
+        print("Must run with args: str, [first, last], int")
+        print("Corresponding to: dataset_name, class_index, K")
+        exit()
+
+    dataset = data_folder + sys.argv[1] + ".data"
+    class_index = sys.argv[2]
+    k = int(sys.argv[3])
+
+    print("Dataset", sys.argv[1])
+    return run(dataset, class_index, k, fn_to_run)
--- run_stackgp.py
+++ run_stackgp.py
@@ -1,80 +1,12 @@
-from sklearn import metrics
-from sklearn.model_selection import KFold
 from src.StackGP import StackGP
-from helpers import read_data
-import time
-import sys
+import helpers
+from functools import partial
 import warnings
 warnings.filterwarnings('ignore')
 
-# The custom name of our method
-pop_size = 10
-num_folds = 10
-data_folder = "datasets/uci/"  # Relative directory
-seed = 0
-
-def evaluation_score(real_y, predicted_y):
-    return metrics.f1_score(real_y, predicted_y, average="weighted")
-
-
-def comparison(train_x, train_y, test_x, test_y):
-
-    # Track the time so we can allocate the comparison methods the same time
-    start_time = time.time()
-
-    method = StackGP(pop_size=pop_size, random_state=seed, verbose=1)
-
-    method.fit(train_x, train_y)
-    training_time_seconds = time.time() - start_time
-
-    training_time_minutes = int(training_time_seconds / 60)
-
-    predictions = method.predict(test_x)
-    score = evaluation_score(test_y, predictions)
-    print("Proposed:", round(score, 3), "and took", training_time_minutes, "minutes")
-
-
-    return [score]
-
-
-def run_kfold(K, dataX, dataY):
-    print("Fold:", K)
-    print("-" * 10)
-
-    # Shuffle the data according to the fixed seed for reproducability
-    kf = KFold(n_splits=num_folds, shuffle=True, random_state=0)
-
-    # We only care about the Kth fold - we run each fold as different process for speed of results
-    train, test = list(kf.split(dataX, dataY))[K]
-
-    train_x, train_y = dataX.iloc[train], dataY.iloc[train]
-    test_x, test_y = dataX.iloc[test], dataY.iloc[test]
-
-    res = comparison(train_x, train_y, test_x, test_y)
-
-    print(res)
-
-    return res
-
-
-def main(dataset, class_index, K):
-    file_name = dataset + ".data"  # Data name with extension added
-    print("Dataset:", dataset)
-    data_x, dataY = read_data(data_folder+file_name, class_index)
-    print("Data Read")
-    run_kfold(K, data_x, dataY)
-
 
 if __name__ == "__main__":
-
-    if len(sys.argv) != 4:
-        print("Must run with args: str, [first, last], int")
-        print("Corresponding to: dataset_name, class_index, K")
-        exit()
-
-
-    dataset = sys.argv[1]
-    class_index = sys.argv[2]
-    K = int(sys.argv[3])
-
-    main(dataset, class_index, K)
+    method = StackGP(pop_size=100,  running_time=0.1, verbose=1)
+    fn = partial(helpers.run_and_time_classifier, method)
+    scores = helpers.main(fn)
+    print("StackGP", scores)
